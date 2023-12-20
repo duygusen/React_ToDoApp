35916dc5ba032320f47ce88d77a9f241d814f3ac
duygusen <seenduygu@gmail.com>
2023-12-20 20:59:24 +0300

    react-intro

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  let totalCount = 0;
+  const [total, setTotal] = useState(0);
+
+  const increase=() => {
+    setTotal(total+1);
+  }
+  const decrease=() => {
+    setTotal(total-1);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <p>Toplama Tıklama Sayısı: {total}</p>
+      <button onClick={() => {
+        increase();
+      }}>+</button>
+      
+      <button onClick={decrease}>-</button>
+
+    </>
   );
 }
 
